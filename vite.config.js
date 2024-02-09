ed72abb89edd9a7ef8b68a1a29ffddc7d43dfbc3
Grigorii K. Shartsev <me@shgk.me>
2024-02-09 13:23:05 +0100

    build: migrate to @nextcloud/vite-config
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue2'
-import browserslistToEsbuild from 'browserslist-to-esbuild'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { createLibConfig } from '@nextcloud/vite-config'
 
-import packageJson from './package.json'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    cssInjectedByJsPlugin(),
-  ],
-  build: {
-    target: browserslistToEsbuild(),
-    lib: {
-      entry: 'src/index.js',
-      name: packageJson.name,
-      fileName: 'index',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: Object.keys(packageJson.peerDependencies)
-    }
-  },
+export default createLibConfig({
+	index: 'src/index.js',
+}, {
+	libraryFormats: ['es'],
+	inlineCSS: true,
 })
